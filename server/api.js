a2ce4c8d40224218896dbf0d99294f58e754212d
Abhishek Kumar Gupta <100848632+Abhigupta13@users.noreply.github.com>
2024-01-23 20:46:55 +0530

    testing

--- server/api.js
+++ server/api.js
@@ -4,50 +4,49 @@ const Crypto = require('./model');
 const mongoose = require('mongoose');
 const apiUrl = 'https://api.wazirx.com/api/v2/tickers';
 
-// Fecth data from api
-// async function fetchAndStoreData() {
-//     try {
-//       const response = await axios.get(apiUrl);
-//       const cryptoData = Object.values(response.data);
+async function fetchAndStoreData() {
+    try {
+      const response = await axios.get(apiUrl);
+      const cryptoData = Object.values(response.data);
 
-//       const top10Data = cryptoData.slice(0, 10);
+      const top10Data = cryptoData.slice(0, 10);
   
-//       await Crypto.deleteMany({}); 
+      await Crypto.deleteMany({}); 
   
-//       for (const crypto of top10Data) {
-//         const {
-//           name,
-//           last,
-//           buy,
-//           sell,
-//           volume,
-//           base_unit,
+      for (const crypto of top10Data) {
+        const {
+          name,
+          last,
+          buy,
+          sell,
+          volume,
+          base_unit,
         
-//         } = crypto;
+        } = crypto;
   
-//         const cryptoData = {
-//           name,
-//           last,
-//           buy,
-//           sell,
-//           volume,
-//           base_unit,
-//         };
+        const cryptoData = {
+          name,
+          last,
+          buy,
+          sell,
+          volume,
+          base_unit,
+        };
   
-//         const newCrypto = new Crypto(cryptoData);
-//         await newCrypto.save();
-//       }
+        const newCrypto = new Crypto(cryptoData);
+        await newCrypto.save();
+      }
   
-//       console.log('Data saved successfully.');
-//     } catch (error) {
-//       console.error('Error fetching and storing data:', error);
-//       res.status(500).json({ error: 'Error fetching and storing data' });
-//     } finally {
-//     //   mongoose.connection.close();
-//     }
-//   }
+      console.log('Data saved successfully.');
+    } catch (error) {
+      console.error('Error fetching and storing data:', error);
+      res.status(500).json({ error: 'Error fetching and storing data' });
+    } finally {
+    //   mongoose.connection.close();
+    }
+  }
   
-//   fetchAndStoreData();
+  fetchAndStoreData();
 
 router.get('/data', async (req, res) => {
     try {
