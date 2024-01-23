c3a87bd7a2c7b349abad37f4aab00a10faeffc32
Abhishek Kumar Gupta <100848632+Abhigupta13@users.noreply.github.com>
2024-01-23 23:20:28 +0530

    deploy changes

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -44,7 +44,7 @@ const [cryptoData, setCryptoData] = useState([]);
 useEffect(() => {
     async function fetchData() {
       try {
-    const response = await axios.get('http://localhost:8080/api/data')
+    const response = await axios.get('https://binance-server1.onrender.com/api/data')
   
     //   console.log("asset",asset);
         const findMatchingObject = (data, valueToMatch) => {
